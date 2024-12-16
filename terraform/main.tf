d429e003678badf98b0746af47c45db2d9d99f0c
Martin Lefler <mlefler@schubergphilis.com>
2024-12-16 10:03:34 +0100

    start with main branch (#2)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 provider "neon" {}
 
-
-
 resource "neon_project" "schema_thesis" {
   for_each = toset(local.schema_versioning_methods)
 
@@ -11,39 +9,19 @@ resource "neon_project" "schema_thesis" {
   region_id                 = "aws-eu-central-1"
 }
 
-resource "neon_branch" "thesis" {
-  for_each = toset(local.schema_versioning_methods)
-
-  project_id = neon_project.schema_thesis[each.key].id
-  parent_id  = neon_project.schema_thesis[each.key].default_branch_id
-  name       = "thesis"
-}
-
 resource "neon_role" "thesis_role" {
   for_each = toset(local.schema_versioning_methods)
 
   project_id = neon_project.schema_thesis[each.key].id
-  branch_id  = neon_branch.thesis[each.key].id
+  branch_id  = neon_project.schema_thesis[each.key].default_branch_id
   name       = "thesis_role"
-
-  depends_on = [
-    neon_endpoint.default
-  ]
-}
-
-resource "neon_endpoint" "default" {
-  for_each = toset(local.schema_versioning_methods)
-
-  project_id = neon_project.schema_thesis[each.key].id
-  branch_id  = neon_branch.thesis[each.key].id
-  type       = "read_write"
 }
 
 resource "neon_database" "movies" {
   for_each = toset(local.schema_versioning_methods)
 
   project_id = neon_project.schema_thesis[each.key].id
-  branch_id  = neon_branch.thesis[each.key].id
+  branch_id  = neon_project.schema_thesis[each.key].default_branch_id
   owner_name = neon_role.thesis_role[each.key].name
   name       = "movies"
 }
