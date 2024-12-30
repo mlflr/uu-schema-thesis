fa418794adf1afaa678120b2404412bb3dc6733e
Martin Lefler <mlefler@schubergphilis.com>
2024-12-30 21:50:20 +0100

    proper connection string

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "connection_string" {
   sensitive = true
   value = {
-    for k in toset(local.schema_versioning_methods) : k => neon_project.schema_thesis[k].connection_uri
+    for k in toset(local.schema_versioning_methods) : k =>
+    "postgresql://${neon_role.thesis_role[k].name}:${neon_role.thesis_role[k].password}@${neon_project.schema_thesis[k].database_host}/${neon_database.movies[k].name}?sslmode=require"
   }
 }
