22aa9bfd13fae92c43b47676fb2480e600ea925d
Martin Lefler <mlefler@schubergphilis.com>
2024-12-31 12:26:38 +0100

    simplified route config

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type movieHandler interface {
+	ListMoviesHandler(w http.ResponseWriter, r *http.Request)
+	CreateMovieHandler(w http.ResponseWriter, r *http.Request)
+	GetMovieHandler(w http.ResponseWriter, r *http.Request)
+	UpdateMovieHandler(w http.ResponseWriter, r *http.Request)
+	DeleteMovieHandler(w http.ResponseWriter, r *http.Request)
+}
+
+type actorHandler interface {
+	ListActorsHandler(w http.ResponseWriter, r *http.Request)
+	CreateActorHandler(w http.ResponseWriter, r *http.Request)
+	GetActorHandler(w http.ResponseWriter, r *http.Request)
+	UpdateActorHandler(w http.ResponseWriter, r *http.Request)
+	DeleteActorHandler(w http.ResponseWriter, r *http.Request)
+}
+
+type personHandler interface {
+	ListPeopleHandler(w http.ResponseWriter, r *http.Request)
+	CreatePersonHandler(w http.ResponseWriter, r *http.Request)
+	GetPersonHandler(w http.ResponseWriter, r *http.Request)
+	UpdatePersonHandler(w http.ResponseWriter, r *http.Request)
+	DeletePersonHandler(w http.ResponseWriter, r *http.Request)
+}
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
@@ -19,4 +44,28 @@ func (app *application) routes() http.Handler {
 	app.routesBranches(router)        // branches routes
 
 	return app.recoverPanic(router)
+}
+
+func registerMovieRoutes(router *httprouter.Router, prefix string, version string, handler movieHandler) {
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/movies", prefix, version), handler.ListMoviesHandler)
+	router.HandlerFunc(http.MethodPost, fmt.Sprintf("%s/%s/movies", prefix, version), handler.CreateMovieHandler)
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/movies/:id", prefix, version), handler.GetMovieHandler)
+	router.HandlerFunc(http.MethodPatch, fmt.Sprintf("%s/%s/movies/:id", prefix, version), handler.UpdateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, fmt.Sprintf("%s/%s/movies/:id", prefix, version), handler.DeleteMovieHandler)
+}
+
+func registerActorRoutes(router *httprouter.Router, prefix string, version string, handler actorHandler) {
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/actors", prefix, version), handler.ListActorsHandler)
+	router.HandlerFunc(http.MethodPost, fmt.Sprintf("%s/%s/actors", prefix, version), handler.CreateActorHandler)
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/actors/:id", prefix, version), handler.GetActorHandler)
+	router.HandlerFunc(http.MethodPatch, fmt.Sprintf("%s/%s/actors/:id", prefix, version), handler.UpdateActorHandler)
+	router.HandlerFunc(http.MethodDelete, fmt.Sprintf("%s/%s/actors/:id", prefix, version), handler.DeleteActorHandler)
+}
+
+func registerPersonRoutes(router *httprouter.Router, prefix string, version string, handler personHandler) {
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/people", prefix, version), handler.ListPeopleHandler)
+	router.HandlerFunc(http.MethodPost, fmt.Sprintf("%s/%s/people", prefix, version), handler.CreatePersonHandler)
+	router.HandlerFunc(http.MethodGet, fmt.Sprintf("%s/%s/people/:id", prefix, version), handler.GetPersonHandler)
+	router.HandlerFunc(http.MethodPatch, fmt.Sprintf("%s/%s/people/:id", prefix, version), handler.UpdatePersonHandler)
+	router.HandlerFunc(http.MethodDelete, fmt.Sprintf("%s/%s/people/:id", prefix, version), handler.DeletePersonHandler)
 }
--- cmd/api/routes_branches.go
+++ cmd/api/routes_branches.go
@@ -1,61 +1,24 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routesBranches(router *httprouter.Router) {
 	// v1/movies routes
-	v1Handler := &app.handlers.Branches.V1.Movies
-	router.HandlerFunc(http.MethodGet, "/branches/v1/movies", v1Handler.ListMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v1/movies", v1Handler.CreateMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v1/movies/:id", v1Handler.GetMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v1/movies/:id", v1Handler.UpdateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v1/movies/:id", v1Handler.DeleteMovieHandler)
+	registerMovieRoutes(router, "branches", "v1", &app.handlers.Branches.V1.Movies)
 
 	// v2/movies routes
-	v2Handler := &app.handlers.Branches.V2.Movies
-	router.HandlerFunc(http.MethodGet, "/branches/v2/movies", v2Handler.ListMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v2/movies", v2Handler.CreateMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v2/movies/:id", v2Handler.GetMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v2/movies/:id", v2Handler.UpdateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v2/movies/:id", v2Handler.DeleteMovieHandler)
+	registerMovieRoutes(router, "branches", "v2", &app.handlers.Branches.V2.Movies)
 
 	// v3/movies routes
-	v3Handler := &app.handlers.Branches.V3.Movies
-	router.HandlerFunc(http.MethodGet, "/branches/v3/movies", v3Handler.ListMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v3/movies", v3Handler.CreateMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v3/movies/:id", v3Handler.GetMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v3/movies/:id", v3Handler.UpdateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v3/movies/:id", v3Handler.DeleteMovieHandler)
+	registerMovieRoutes(router, "branches", "v3", &app.handlers.Branches.V3.Movies)
 
-	// v4/movies routes
-	v4Handler := &app.handlers.Branches.V4.Movies
-	v4ActorHandler := &app.handlers.Branches.V4.Actors
-	router.HandlerFunc(http.MethodGet, "/branches/v4/movies", v4Handler.ListMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v4/movies", v4Handler.CreateMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v4/movies/:id", v4Handler.GetMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v4/movies/:id", v4Handler.UpdateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v4/movies/:id", v4Handler.DeleteMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v4/actors", v4ActorHandler.ListActorsHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v4/actors", v4ActorHandler.CreateActorHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v4/actors/:id", v4ActorHandler.GetActorHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v4/actors/:id", v4ActorHandler.UpdateActorHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v4/actors/:id", v4ActorHandler.DeleteActorHandler)
+	// v4/movies and actors routes
+	registerMovieRoutes(router, "branches", "v4", &app.handlers.Branches.V4.Movies)
+	registerActorRoutes(router, "branches", "v4", &app.handlers.Branches.V4.Actors)
 
-	// v5/movies routes
-	v5Handler := &app.handlers.Branches.V5.Movies
-	v5PersonHandler := &app.handlers.Branches.V5.People
-	router.HandlerFunc(http.MethodGet, "/branches/v5/movies", v5Handler.ListMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v5/movies", v5Handler.CreateMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v5/movies/:id", v5Handler.GetMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v5/movies/:id", v5Handler.UpdateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v5/movies/:id", v5Handler.DeleteMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v5/people", v5PersonHandler.ListPeopleHandler)
-	router.HandlerFunc(http.MethodPost, "/branches/v5/people", v5PersonHandler.CreatePersonHandler)
-	router.HandlerFunc(http.MethodGet, "/branches/v5/people/:id", v5PersonHandler.GetPersonHandler)
-	router.HandlerFunc(http.MethodPatch, "/branches/v5/people/:id", v5PersonHandler.UpdatePersonHandler)
-	router.HandlerFunc(http.MethodDelete, "/branches/v5/people/:id", v5PersonHandler.DeletePersonHandler)
+	// v5/movies and people routes
+	registerMovieRoutes(router, "branches", "v5", &app.handlers.Branches.V5.Movies)
+	registerPersonRoutes(router, "branches", "v5", &app.handlers.Branches.V5.People)
 }
